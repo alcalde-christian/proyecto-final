f665a08bd566267ec707c5b125fde7d1ee1b5536
Christian Alcalde <alcalde_christian@hotmail.com>
2024-03-18 14:53:06 -0300

    Primer commit

---
+++ forms/form.php
@@ -0,0 +1,25 @@
+<?php
+
+// Creación de variables para rescatar los datos del html
+$name = $_POST ['name'];
+$email = $_POST ['email'];
+$phone = $_POST ['phone'];
+$text = $_POST ['text'];
+
+// Cuerpo del mail
+$text = "Has recibido un nuevo mensaje de " . $name . "\r\n";
+$text .= "Mail: " . $email . "\r\n";
+$text .= "Teléfono: " . $phone . "\r\n";
+$text .= $_POST ['text'] . ".\r\n";
+$text .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'alcalde_christian@hotmail.com'; // Mail receptor de mensajes 
+$asunto = 'Mail enviado a través de la web de Estudio de Arquitectura'; // Asunto del mail
+
+// Función mail para el envío del mismo
+mail($para, $asunto, utf8_decode($text), $header);
+
+// Redirección al html de "envío exitoso"
+header('Location:envio.html')
+
+?>
